208ca3152c2e81ef8d5907bcc127253a1acaf6b0
jcausey <jcausey@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-08-21 14:23:18 +0000

    second take at nagios warnings for collector installs; warns user about missing binaries when installing remote collectors; reviewed by jhanson
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.NtpMonitor@63081 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/NtpMonitor/__init__.py
+++ ZenPacks/zenoss/NtpMonitor/__init__.py
@@ -2,7 +2,19 @@
 import Globals
 import os.path
 
+import logging
+log = logging.getLogger("zen.NtpMonitor")
+
 skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
 from Products.CMFCore.DirectoryView import registerDirectory
 if os.path.isdir(skinsDir):
     registerDirectory(skinsDir, globals())
+
+def onCollectorInstalled(ob, event):
+    zpFriendly = 'NtpMonitor'
+    errormsg = '{0} binary cannot be found on {1}. It must be installed before {2} can function.'
+    
+    verifyBin = 'check_ntp'
+    code, output = ob.executeCommand('zenbincheck %s' % verifyBin, 'zenoss', needsZenHome=True)
+    if code:
+       	log.warn(errormsg.format(verifyBin, ob.hostname, zpFriendly))
