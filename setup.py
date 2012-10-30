c67d664b8d8674de0f5452a405ec730b54d33feb
jcausey <jcausey@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-10-30 22:41:54 +0000

    Bump zenpack versions for those zenpacks that have changed since 4.2.0; reviewed by jpeacock
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.NtpMonitor@65572 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # These variables are overwritten by Zenoss when the ZenPack is exported
 # or saved.  Do not modify them directly here.
 NAME = 'ZenPacks.zenoss.NtpMonitor'
-VERSION = '2.1.1'
+VERSION = '2.2.0'
 AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
