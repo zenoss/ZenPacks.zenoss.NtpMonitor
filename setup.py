d54f63fb86026816bfa8410bfec6f1af07362619
Dmytro Budko <hoster@mail.rv.ua>
2017-12-06 17:07:32 +0000

    post release: 2.2.3 -> 2.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # These variables are overwritten by Zenoss when the ZenPack is exported
 # or saved.  Do not modify them directly here.
 NAME = 'ZenPacks.zenoss.NtpMonitor'
-VERSION = '2.2.3'
+VERSION = '2.2.4'
 AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
