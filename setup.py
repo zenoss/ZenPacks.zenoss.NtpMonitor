cf7a460da18a6ed72d01b9053ea14bea10d7e8f1
Philip Carinhas <pcarinhas@zenoss.com>
2018-05-18 16:55:08 -0500

    post release: 3.0.0 -> 3.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # These variables are overwritten by Zenoss when the ZenPack is exported
 # or saved.  Do not modify them directly here.
 NAME = 'ZenPacks.zenoss.NtpMonitor'
-VERSION = '3.0.0'
+VERSION = '3.1.0'
 AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
